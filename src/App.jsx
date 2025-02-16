6f25d18c9a33d35022b0240f1dd655bad8214550
Shariif-Mohsoo <mr8781256@gmail.com>
2025-02-16 10:32:41 +0500

    Main Entry Point

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritiesPage from "./pages/Favorities";
-
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 const App = function () {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route exact path="/" element={<AllMeetupsPage />} />
         <Route exact path="/new-meetup" element={<NewMeetupPage />} />
         <Route exact path="/favorites" element={<FavoritiesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 };
 export default App;
+
+// 1  https://react-app-a-2216a-default-rtdb.firebaseio.com
+// 2  https://react-app-a-2216a-default-rtdb.firebaseio.com/
