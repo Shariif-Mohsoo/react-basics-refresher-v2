59be151f3150b3254a2ec9f03ad2ef5a0e8c09bd
Shariif-Mohsoo <mr8781256@gmail.com>
2025-02-16 10:32:22 +0500

    To add meetup

--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,5 +1,31 @@
+import { useNavigate } from "react-router-dom";
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 const NewMeetupPage = () => {
-  return <div>NewMeetupPage</div>;
+  const navigate = useNavigate();
+  function addMeetupHandler(meetupData) {
+    console.log(meetupData);
+    //react-app-a-2216a-default-rtdb.firebaseio.com/dbName.json
+    https: fetch(
+      "https://react-app-a-2216a-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      navigate("/");
+    });
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 };
 
 export default NewMeetupPage;
