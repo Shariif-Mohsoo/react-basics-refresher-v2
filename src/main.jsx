af1f6e3d30f0d75cb5c708c9e01e7d74ab4ba3b3
Shariif-Mohsoo <mr8781256@gmail.com>
2025-02-18 07:51:05 +0500

    Main point

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { FavoritesContextProvider } from "./store/favorities-context.jsx";
 
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <FavoritesContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FavoritesContextProvider>
   </StrictMode>
 );
