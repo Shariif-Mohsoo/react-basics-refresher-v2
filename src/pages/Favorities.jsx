650ce39c19d7e4e10c5809ba6d0cbb9fff64a01a
Shariif-Mohsoo <mr8781256@gmail.com>
2025-02-18 07:52:23 +0500

    My Favorties

--- src/pages/Favorities.jsx
+++ src/pages/Favorities.jsx
@@ -1,5 +1,21 @@
+import { useContext } from "react";
+import FavoritiesContext from "../store/favorities-context";
+import MeetupList from "../components/meetups/MeetupList";
+
 const FavoritiesPage = () => {
-  return <div>FavoritiesPage</div>;
+  const favoritiesCtx = useContext(FavoritiesContext);
+  let content;
+  if (favoritiesCtx.totalFavorities) {
+    content = <MeetupList meetups={favoritiesCtx.favorities} />;
+  } else {
+    content = <p>You got no favorites yet. Start adding some?</p>;
+  }
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
 };
 
 export default FavoritiesPage;
