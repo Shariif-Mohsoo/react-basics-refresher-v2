a217cc8ed5ccc5c2b1798de7366256af59e800e0
Shariif-Mohsoo <mr8781256@gmail.com>
2025-02-23 11:38:45 +0500

    Main Entry Point

--- Eat-N-Splits/src/main.jsx
+++ Eat-N-Splits/src/main.jsx
@@ -1,7 +1,8 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import App from "./App";
 import "./index.css";
-import { StrictMode } from "react";
 
 const el = document.getElementById("root");
 const root = createRoot(el);
