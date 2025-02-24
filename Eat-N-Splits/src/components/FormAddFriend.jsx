0cd53d9c743a04001a52420b6e09b7471cda69bb
Shariif-Mohsoo <mr8781256@gmail.com>
2025-02-24 07:23:11 +0500

    Form to create friend

---
+++ Eat-N-Splits/src/components/FormAddFriend.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Button from "./Button";
+
+const FormAddFriend = ({ onAddFriend }) => {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?u=${id}`,
+      balance: 0,
+    };
+    // console.log(newFriend);
+    onAddFriend(newFriend);
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
+
+  return (
+    <form className="form-add-friend" onSubmit={handleSubmit}>
+      <label>Friend Name</label>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        autoFocus
+        required
+      />
+
+      <label>Image URL</label>
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+        required
+      />
+
+      <Button>Add</Button>
+    </form>
+  );
+};
+
+export default FormAddFriend;
